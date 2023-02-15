49ea6cdcecea9ae2df58da9e6bb55799f258b382
Dmitriy Emelyanov <dmitriyeee@gmail.com>
2023-02-15 13:15:38 +0300

    Выволнение 4 задачи

---
+++ Homework1/Task4/t4.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+while (index <= N)
+{
+  if (index % 2 == 0)
+  {
+    Console.WriteLine(index);
+  }
+  index = index + 1;
+}
+Console.Write("End");
