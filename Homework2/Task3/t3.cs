35f5a484b91336e79b091c51fd68ba6d246c31ed
Dmitriy Emelyanov <dmitriyeee@gmail.com>
2023-02-27 19:47:50 +0300

    Решение 3 задачи 2-ого ДЗ

---
+++ Homework2/Task3/t3.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+void array(int N)
+{
+    if (N == 6 || N == 7)
+    {
+        Console.WriteLine("Да.");
+    }
+    else
+    {
+        Console.WriteLine("Нет.");
+    }
+}
+Console.Write("Введите номер дня недели: ");
+int numberDay = Convert.ToInt32(Console.ReadLine());
+if (numberDay > 7 || numberDay < 1)
+{
+    Console.WriteLine("Операция невозможна.");
+    return;
+}
+array (numberDay);
