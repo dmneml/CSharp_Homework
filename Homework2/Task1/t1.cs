c24b9443ab7ab9e5128886573bfdf5179d51c664
Dmitriy Emelyanov <dmitriyeee@gmail.com>
2023-02-27 17:46:53 +0300

    Решение 1 задачи 2-ого ДЗ

---
+++ Homework2/Task1/t1.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = N/100;
+int c = N%10;
+int b = (N - a*100 - c)/10;
+Console.WriteLine(b);
