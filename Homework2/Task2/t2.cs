a032e5f99108cefd251ef9ebf05fcdcd19ac5571
Dmitriy Emelyanov <dmitriyeee@gmail.com>
2023-02-27 19:23:00 +0300

    Решение 2 задачи 2-ого ДЗ

---
+++ Homework2/Task2/t2.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+int showThirdDigit(int number)
+{
+    int thirdDigit = 0;
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    thirdDigit = number % 10;
+    return thirdDigit;
+}
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number / 100 == 0)
+{
+    Console.WriteLine("Третьей цифры нет.");
+    return;
+}
+int result = showThirdDigit(number);
+Console.WriteLine(result);
+
+//Очень долго мучился с этой задачей, пока не нашел метод через Digit 
