d2d8a85af62ca24447bc37e625252033579949ed
Dmitriy Emelyanov <dmitriyeee@gmail.com>
2023-03-20 17:13:37 +0300

    Выполнение 9 ДЗ

---
+++ Homework9/Task2/t66.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N. Выполнить с помошью рекурсии.
+
+// Провести расчет от M и N
+
+int CalculateSum(int min, int max)
+{
+  if (min == max) return max;
+  if (min > max) return 0;
+  else return min + CalculateSum(min + 1, max);
+}
+
+int n = 1;
+int m = 10;
+Console.WriteLine($"Сумма чисел от {n} до {m}: {CalculateSum(n, m)}.");
