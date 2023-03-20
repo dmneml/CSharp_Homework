5f5bbaeeb08960985f26ac48a8cb247d99b86fbb
Dmitriy Emelyanov <dmitriyeee@gmail.com>
2023-03-20 15:31:03 +0300

    Выполнение 8 ДЗ

---
+++ Homework8/Task1/t54.cs
@@ -0,0 +1,66 @@
+﻿// Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+// Создать двойной массив
+
+int[,] CreateTwoDimArray(int row, int column)
+{
+  return new int[row, column];
+}
+
+// Заполнение массива случайными числами
+
+void FillTwoDimArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(1, 10);
+    }
+  }
+}
+
+// Печать массива
+
+void PrintDimArray(int[,] array)
+{
+  Console.WriteLine(" ");
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write($" {array[i, j]} ");
+    }
+    Console.WriteLine(" ");
+  }
+}
+
+// Сортировка строк (по убыванию)
+void SortDimArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int n = 0; n < array.GetLength(1)-1; n++)
+    { 
+    for (int j = 0; j < array.GetLength(1)-1; j++)
+    {
+      if (array[i, j] < array[i, j + 1])
+      {
+        int temp = array[i, j];
+        array[i, j] = array[i, j + 1];
+        array[i, j + 1] = temp;
+      }
+    }
+    }
+  }
+}
+
+
+int rows = new Random().Next(2, 10);
+int columns = new Random().Next(2, 10);
+int[,] userArray = CreateTwoDimArray(rows, columns);
+FillTwoDimArray(userArray);
+PrintDimArray(userArray);
+SortDimArray(userArray);
+PrintDimArray(userArray);
---
+++ Homework8/Task2/t56.cs
@@ -0,0 +1,94 @@
+﻿// Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Создать двумерный массив
+
+int[,] CreateTwoDimArray(int row, int column)
+{
+  return new int[row, column];
+}
+
+// Заполнение  массива случайными числами
+
+void FillTwoDimArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(1, 10);
+    }
+  }
+}
+
+// Посчитать сумму элементов в каждой строке
+
+int[] SumLineCalculations(int[,] array)
+{
+  int[] sumInLine = new int[array.GetLength(0)];
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      sumInLine[i] = sumInLine[i] + array[i, j];
+    }
+  }
+  return sumInLine;
+}
+
+// Поиск минимального среди сумм строк массива
+
+string FindMinInArray(int[] array)
+{
+  int minIndex = 0;
+  int minSum = array[minIndex];
+  string sumIndices = string.Empty;
+  for (int i = 1; i < array.Length; i++)
+  {
+    if (array[i] == minSum)
+    {
+      sumIndices = sumIndices + ", " + Convert.ToString(i + 1);
+    }
+    else if (array[i] < minSum)
+    {
+      minSum = array[i];
+      minIndex = i;
+      sumIndices = Convert.ToString(minIndex + 1);
+    }
+    else if (minSum == array[0] && array[i] == minSum)
+    {
+      sumIndices = sumIndices + ", " + Convert.ToString(i + 1);
+    }
+    else if (minSum == array[0])
+    {
+      sumIndices = Convert.ToString(minIndex + 1);
+    }
+  }
+  return sumIndices;
+}
+
+// Печать массива с результатом по каждой строке
+
+void PrintDimArray(int[,] array, int[] sumArray)
+{
+  Console.WriteLine(" ");
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write($" {array[i, j]} ");
+    }
+    Console.Write($"= {sumArray[i]} ({i + 1} line)");
+    Console.WriteLine();
+  }
+}
+
+int rows = new Random().Next(2, 10);
+int columns = new Random().Next(2, 10);
+int[,] userArray = CreateTwoDimArray(rows, columns);
+FillTwoDimArray(userArray);
+int[] sumArray = SumLineCalculations(userArray);
+PrintDimArray(userArray, sumArray);
+string min = FindMinInArray(sumArray);
+Console.WriteLine($"Минимальная сумма элементов линии {min}.");
+
