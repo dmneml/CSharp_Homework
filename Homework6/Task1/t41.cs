c521de25d683ea9e402f5650170f133e09be83cb
Dmitriy Emelyanov <dmitriyeee@gmail.com>
2023-03-20 10:51:25 +0300

    Выполнение 6 ДЗ

---
+++ Homework6/Task1/t41.cs
@@ -0,0 +1,41 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// Определение количества чисел для ввода
+
+int GetIntNumber(string text)
+{
+  Console.WriteLine(text);
+  return Convert.ToInt32(Console.ReadLine());
+}
+
+// Заполнение массива пользователем
+
+int[] UserNumbers(int sizeNum)
+{
+  int[] numbers = new int[sizeNum];
+  for (int i = 0; i < sizeNum; i++)
+  {
+    numbers[i] = GetIntNumber($"Введите {i + 1} число: ");
+  }
+  return numbers;
+}
+
+// Проверка числа на условие задачи
+
+int CheckAndCount(int[] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] > 0)
+    {
+      count++;
+    }
+  }
+  return count;
+}
+
+
+int size = GetIntNumber("Введите количесво чисел: ");
+int[] userNumbers = UserNumbers(size);
+Console.WriteLine($"Количество чисел в массиве, больше 0: {CheckAndCount(userNumbers)}.");
---
+++ Homework6/Task2/t43.cs
@@ -0,0 +1,52 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// Заполнение размера массива
+
+double GetIntNumber(string text)
+  {
+    Console.Write(text);
+    return Convert.ToDouble(Console.ReadLine());
+  }
+
+// Создание массива
+
+double[] CreateIntArray()
+  {
+    return new double[4];
+  }
+
+// Ввод значений пользователем
+
+void FillByUserNumbersArray(string[]text, double[]numbers)
+{
+  Console.WriteLine("Ввод значения: ");
+  for (int i = 0; i < text.Length; i++)
+  {
+  numbers[i] = GetIntNumber(text[i]);
+  }
+}
+
+// Расчет X
+
+double CalculationX(double[]array)
+{
+  double result = Math.Round(Convert.ToDouble((array[2]-array[0])/(array[1]-array[3])),1);
+  return result;
+}
+// Расчет Y
+
+double CalculationY(double num, double[] array)
+{
+  double result = Math.Round(Convert.ToDouble(array[1]*num+array[0]),1);
+  return result;
+}
+
+Console.WriteLine ("Нахождение точки пересечения двух прямых, заданных уравнениями: ");
+Console.WriteLine ("y = k1 * x + b1 и y = k2 * x + b2.");
+string[] variables = new string[4] {"b1 = ","k1 = ","b2 = ","k2 = "};
+double [] userNumbers = CreateIntArray();
+FillByUserNumbersArray (variables, userNumbers);
+double piercingX = CalculationX(userNumbers);
+double piercingY = CalculationY(piercingX, userNumbers);
+Console.WriteLine ($"Точка пересечения: ({piercingX}, {piercingY}).");
